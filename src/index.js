200140e5a16caa0943165ad0c604d341e588583d
AdrianTheHacker <adriantarantino2006@gmail.com>
2022-12-11 15:05:09 -0500

    Finishing the buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class Block extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: this.props.value,
+      position: this.props.position,
+    };
+  }
+
+  render() {
+    const divPosition = `Button ${this.state.position}`;
+
+    return (
+      <div class={divPosition}>
+        <p class='Text'>{this.state.value}</p>
+      </div>
+    )
+  }
+}
+
+
+class Screen extends React.Component {
+  renderBlock(value, position) {
+    return (
+      <Block value={value} position={position}/>
+    );
+  }
+  
+  render() {
+    return (
+      <div class='Screen'>
+        <div class='Row'>
+          {this.renderBlock(7, "one")}
+          {this.renderBlock(8, "two")}
+          {this.renderBlock(9, "three")}
+        </div>
+        <div class='Row'>
+          {this.renderBlock(4, "one")}
+          {this.renderBlock(5, "two")}
+          {this.renderBlock(6, "three")}
+        </div>
+        <div class='Row'>
+          {this.renderBlock(1, "one")}
+          {this.renderBlock(2, "two")}
+          {this.renderBlock(3, "three")}
+        </div>
+        <div class='Row'>
+          {this.renderBlock(0, "one")}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <Screen/>
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Calculator/>);
